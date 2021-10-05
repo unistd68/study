ae045a18dd4c797ae54430f4c825ba2234909271
unistd68 <unistd68@gmail.com>
2021-10-05 10:41:50 +0800

    fix bugs

--- auto_build.sh
+++ auto_build.sh
@@ -1,7 +1,7 @@
 ###
  # @Author: your name
  # @Date: 2021-10-03 16:16:54
- # @LastEditTime: 2021-10-03 16:53:06
+ # @LastEditTime: 2021-10-05 10:41:25
  # @LastEditors: Please set LastEditors
  # @Description: In User Settings Edit
  # @FilePath: \dubbo-goe:\code\study\auto_build.sh
@@ -18,12 +18,13 @@ if [ $sid  ]
 then
   sudo kill -9 $sid
 fi
-sudo rm -f my_http
-sudo rm -f /home/xtcgch/MyService/my_http
+sudo /bin/rm -f my_http
+sudo /bin/rm -f /home/xtcgch/MyService/my_http
 echo "* begin make *"
 make
 echo "* files *"
 sudo /bin/cp -f my_http /home/xtcgch/MyService/my_http
-(nohup /home/xtcgch/MyService/my_http &)
+daemonize -E BUILD_ID=dontKillMe (nohup /home/xtcgch/MyService/my_http &)
 echo "### build finished ! ###"
 ps -ef | grep my_http
+sleep 5s
