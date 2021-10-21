dcf3dc2d83d2ca67b6d8cfd90fec208916c06fd3
unistd68 <unistd68@gmail.com>
2021-10-22 00:03:50 +0800

    1

--- libs/unitTest/sm4/test_sm4.cpp
+++ libs/unitTest/sm4/test_sm4.cpp
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-10-16 17:27:52
- * @LastEditTime: 2021-10-21 23:58:39
+ * @LastEditTime: 2021-10-22 00:03:36
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \dubbo-goe:\code\study\sm4\TestSM4\test_sm4.cpp
@@ -17,11 +17,11 @@ const char* encrpto_data = "C3A6D7F25CD94E43525A8DEFA20B7F5DBCAFF7DFD1AF14E08386
 //加密前: 7B226E616D65223A22E5BCA0E4B889222C22616765223A223138222C22736578223A226D616C65227D
 //加密后: 82ADED14217761441B4F1BFE1E26390700FC5D1E4E85D24D5AB44B8FF4AD01FA0900F6E43D08C409ED1CFBCF5283B399
 
-void testSM4Encrypto(const char *in,const char *key)
+void testSM4Encrypto()
 {
-    //const char *key = "1234567890abcdef";
+    const char *key = "1234567890abcdef";
     //const char *in = "{\"name\":\"张三\",\"age\":\"18\",\"sex\":\"male\"}";
-    //const char *in = "wqetwdadada";
+    const char *in = "wqetwdadada";
     char out[1024] = {0};
     SM4Encrypto(key, in, out);
     printf("key: %s\n",key);
@@ -55,7 +55,7 @@ void testSM4Decrypto()
 
 int main(char *argv[],int argc)
 {
-    testSM4Encrypto(argv[0],argv[1]);
+    testSM4Encrypto();
     //testSM4Decrypto();
     return 0;
 }
