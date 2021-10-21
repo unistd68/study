6b930fc51e4aebfeb8d2f04912026401b11c530a
unistd68 <unistd68@gmail.com>
2021-10-22 00:25:09 +0800

    1

--- libs/unitTest/sm4/test_sm4.cpp
+++ libs/unitTest/sm4/test_sm4.cpp
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-10-16 17:27:52
- * @LastEditTime: 2021-10-22 00:21:29
+ * @LastEditTime: 2021-10-22 00:25:00
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \dubbo-goe:\code\study\sm4\TestSM4\test_sm4.cpp
@@ -9,10 +9,10 @@
 
 #include "sm4.h"
 
-const char* key = "123456789gabcdef";
-const char* plain_in = "{\"name\":\"zhangsan\",\"age\":\"18\",\"sex\":\"male\"}";
-const char* plain_in_hex = "7B226E616D65223A227A68616E6773616E222C22616765223A223138222C22736578223A226D616C65227D";
-const char* encrpto_data = "C3A6D7F25CD94E43525A8DEFA20B7F5DBCAFF7DFD1AF14E08386116D1300F1CCDB44E374DD6731221BE8B0D5A3A02F85";
+// const char* key = "123456789gabcdef";
+// const char* plain_in = "{\"name\":\"zhangsan\",\"age\":\"18\",\"sex\":\"male\"}";
+// const char* plain_in_hex = "7B226E616D65223A227A68616E6773616E222C22616765223A223138222C22736578223A226D616C65227D";
+// const char* encrpto_data = "C3A6D7F25CD94E43525A8DEFA20B7F5DBCAFF7DFD1AF14E08386116D1300F1CCDB44E374DD6731221BE8B0D5A3A02F85";
 //加密前: {"name":"张三","age":"18","sex":"male"}
 //加密前: 7B226E616D65223A22E5BCA0E4B889222C22616765223A223138222C22736578223A226D616C65227D
 //加密后: 82ADED14217761441B4F1BFE1E26390700FC5D1E4E85D24D5AB44B8FF4AD01FA0900F6E43D08C409ED1CFBCF5283B399
