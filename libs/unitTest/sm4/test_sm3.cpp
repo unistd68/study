f24a0276226063f5e381ad7feace07fae5726356
unistd68 <unistd68@gmail.com>
2021-10-21 23:23:20 +0800

    1

--- libs/unitTest/sm4/test_sm3.cpp
+++ libs/unitTest/sm4/test_sm3.cpp
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-10-21 20:13:17
- * @LastEditTime: 2021-10-21 21:42:27
+ * @LastEditTime: 2021-10-21 23:23:14
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \dubbo-goe:\code\study\libs\unitTest\sm4\test_sm3.cpp
@@ -12,7 +12,7 @@
 
 void test()
 {
-    const unsigned char *in = "{\"name\":\"zhangsan\",\"age\":\"18\",\"sex\":\"male\"}";
+    const unsigned char *in = "abc";
     unsigned char out[1024] = {0};
     printf("in:%s\n", in);
     printf("len:%d\n", strlen(in));
