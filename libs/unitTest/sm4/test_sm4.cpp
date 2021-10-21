f63202fd0ef2087cde7b576091d27e6c5fcfc228
unistd68 <unistd68@gmail.com>
2021-10-21 23:58:53 +0800

    1

--- libs/unitTest/sm4/test_sm4.cpp
+++ libs/unitTest/sm4/test_sm4.cpp
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-10-16 17:27:52
- * @LastEditTime: 2021-10-16 17:27:53
+ * @LastEditTime: 2021-10-21 23:58:39
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \dubbo-goe:\code\study\sm4\TestSM4\test_sm4.cpp
@@ -17,20 +17,24 @@ const char* encrpto_data = "C3A6D7F25CD94E43525A8DEFA20B7F5DBCAFF7DFD1AF14E08386
 //加密前: 7B226E616D65223A22E5BCA0E4B889222C22616765223A223138222C22736578223A226D616C65227D
 //加密后: 82ADED14217761441B4F1BFE1E26390700FC5D1E4E85D24D5AB44B8FF4AD01FA0900F6E43D08C409ED1CFBCF5283B399
 
-void testSM4Encrypto()
+void testSM4Encrypto(const char *in,const char *key)
 {
-    // const char *key = "1234567890abcdef";
-    // const char *in = "{\"name\":\"张三\",\"age\":\"18\",\"sex\":\"male\"}";
+    //const char *key = "1234567890abcdef";
+    //const char *in = "{\"name\":\"张三\",\"age\":\"18\",\"sex\":\"male\"}";
+    //const char *in = "wqetwdadada";
     char out[1024] = {0};
-    SM4Encrypto(key, plain_in, out);
-    if(strcmp(encrpto_data,out) == 0)
-    {
-        printf(" ### testSM4Encrypto ### test ok\n");
-    }
-    else
-    {
-        printf(" ### testSM4Encrypto ### test fail\n");
-    }
+    SM4Encrypto(key, in, out);
+    printf("key: %s\n",key);
+    printf("in: %s\n",in);
+    printf("out: %s\n",out);
+    // if(strcmp(encrpto_data,out) == 0)
+    // {
+    //     printf(" ### testSM4Encrypto ### test ok\n");
+    // }
+    // else
+    // {
+    //     printf(" ### testSM4Encrypto ### test fail\n");
+    // }
 }
 
 void testSM4Decrypto()
@@ -49,9 +53,9 @@ void testSM4Decrypto()
     }
 }
 
-int main()
+int main(char *argv[],int argc)
 {
-    testSM4Encrypto();
-    testSM4Decrypto();
+    testSM4Encrypto(argv[0],argv[1]);
+    //testSM4Decrypto();
     return 0;
 }
