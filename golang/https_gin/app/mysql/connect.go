428268f5328e930458d5cffbcc8d06f273fbfb9e
unistd68 <unistd68@gmail.com>
2021-11-08 18:13:21 +0800

    1

--- golang/https_gin/app/mysql/connect.go
+++ golang/https_gin/app/mysql/connect.go
@@ -2,9 +2,9 @@ package db_mysql
 
 import (
 	"fmt"
-    // "database/sql"
-	"github.com/jmoiron/sqlx"
-    // "github.com/go-sql-driver/mysql"
+    "database/sql"
+	// "github.com/jmoiron/sqlx"
+    _ "github.com/go-sql-driver/mysql"
 	model "https_gin/app/model"
 )
 
@@ -12,8 +12,12 @@ func Connect(dbinfo model.DBAuthInfo)  {
 	var url =fmt.Sprintf("%s:%s@tcp(%s:%s)/%s",dbinfo.DBUser,dbinfo.DBPwd,dbinfo.DBHost,dbinfo.DBPort,dbinfo.DBName)
 	fmt.Println(url)
 
+	db, _ = sql.Open("mysql", "gch:GCHgch_123456@tcp(127.0.0.1:3306)/test?charset=utf8")
+    db.SetMaxOpenConns(2000)
+    db.SetMaxIdleConns(1000)
+    // db.Ping()
 	// db,_:=sql.Open("mysql",string(url)) // 设置连接数据库的参数
-	db,_:=sqlx.Open("mysql","gch:GCHgch_123456@tcp(127.0.0.1:3306)/test")
+	// db,_:=sqlx.Open("mysql","gch:GCHgch_123456@tcp(127.0.0.1:3306)/test")
     //defer db.Close()    //关闭数据库
     err:=db.Ping()      //连接数据库
     if err!=nil{
@@ -21,7 +25,7 @@ func Connect(dbinfo model.DBAuthInfo)  {
         return
     }
 	fmt.Println("数据库连接成功")
-	db.Close()
+	// db.Close()
 }
 
 func query(){}
