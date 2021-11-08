b881afbab8c7c4580fe386abc58368a46a478458
unistd68 <unistd68@gmail.com>
2021-11-08 12:58:49 +0800

    test

--- golang/gin/main.go
+++ golang/gin/main.go
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-11-08 09:56:58
- * @LastEditTime: 2021-11-08 11:05:46
+ * @LastEditTime: 2021-11-08 12:57:46
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \dubbo-goe:\other\vshare\test\golang\gin\main.go
@@ -55,7 +55,7 @@
 	// 初始化路由
 	r := routers.Init()
 	if err := r.Run(":8888"); err != nil {
-		fmt.Println("startup service failed, err:%v\n", err)
+		fmt.Println(" startup service failed, err:%v\n", err)
 	}
 }
 
