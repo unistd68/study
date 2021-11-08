4ea6298c4e59a3cf484d77c8a9639cf69db4deac
unistd68 <unistd68@gmail.com>
2021-11-08 13:24:28 +0800

    fix

--- python/main.py
+++ python/main.py
@@ -1,10 +1,10 @@
 '''
 Author: your name
 Date: 2021-11-08 13:05:21
-LastEditTime: 2021-11-08 13:21:00
+LastEditTime: 2021-11-08 13:24:01
 LastEditors: Please set LastEditors
-Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
-FilePath: \dubbo-goe:\other\vshare\test\python\main.py
+Description: 
+FilePath: \other\vshare\test\python\main.py
 '''
 
 import ConfigParser
