078bc0b038170207b45b14b50e7c610c39ee9128
unistd68 <unistd68@gmail.com>
2021-11-17 12:52:29 +0800

    1

--- leetcode/下一个排列/main.cpp
+++ leetcode/下一个排列/main.cpp
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-11-17 11:55:06
- * @LastEditTime: 2021-11-17 12:43:00
+ * @LastEditTime: 2021-11-17 12:44:04
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \dubbo-goe:\other\vshare\test\leetcode\下一个排列\main.cpp
@@ -36,8 +36,9 @@ void test()
         std::cin >> tmp;
         nums.emplace_back(tmp);
     }
+    show_vector(nums,"排序前");
     solution.nextPermutation(nums);
-    show_vector(nums,"下一个排列");
+    show_vector(nums,"排序后");
 }
 
 void show_help()
