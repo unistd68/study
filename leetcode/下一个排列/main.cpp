2893167e85ee39c2e54faa9fb40d280ee376d82d
unistd68 <unistd68@gmail.com>
2021-11-17 12:40:52 +0800

    1

--- leetcode/下一个排列/main.cpp
+++ leetcode/下一个排列/main.cpp
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-11-17 11:55:06
- * @LastEditTime: 2021-11-17 12:36:31
+ * @LastEditTime: 2021-11-17 12:40:44
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \dubbo-goe:\other\vshare\test\leetcode\下一个排列\main.cpp
@@ -59,7 +59,7 @@ void do_something(int argc, char *argv[])
 	{
 		
 	}
-	else if (argc == 4 && strcmp(argv[1], "3") == 0)
+	else if (argc == 2 && strcmp(argv[1], "3") == 0)
 	{
 		printf("################# 测试 开始 ############################ \n");
 		test();
