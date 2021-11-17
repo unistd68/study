e8a88f21331a7645fd2316b9845954161b98e1aa
unistd68 <unistd68@gmail.com>
2021-11-17 12:43:05 +0800

    1

--- leetcode/下一个排列/main.cpp
+++ leetcode/下一个排列/main.cpp
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-11-17 11:55:06
- * @LastEditTime: 2021-11-17 12:40:44
+ * @LastEditTime: 2021-11-17 12:43:00
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \dubbo-goe:\other\vshare\test\leetcode\下一个排列\main.cpp
@@ -32,7 +32,9 @@ void test()
     for (int i = 0; i < n; i++)
     {
         std::cout << "请输入第" << i + 1 << "个数：" << std::endl;
-        std::cin >> nums[i];
+        int tmp;
+        std::cin >> tmp;
+        nums.emplace_back(tmp);
     }
     solution.nextPermutation(nums);
     show_vector(nums,"下一个排列");
