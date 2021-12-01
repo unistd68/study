7252a565603f257bdf0d2364a8b9589206d39d60
unistd68 <unistd68@gmail.com>
2021-12-01 11:22:05 +0800

    1

--- web/ant-design-pro-master/config/proxy.ts
+++ web/ant-design-pro-master/config/proxy.ts
@@ -11,7 +11,8 @@ export default {
     // localhost:8000/api/** -> https://preview.pro.ant.design/api/**
     '/api/': {
       // 要代理的地址
-      target: 'https://preview.pro.ant.design',
+      // target: 'https://preview.pro.ant.design',
+      target: 'localhost:8888/api/',
       // 配置了这个可以从 http 代理到 https
       // 依赖 origin 的功能可能需要这个，比如 cookie
       changeOrigin: true,
